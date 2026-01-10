4f74ccc57b693e2dd2b0aa3c682c885c5b2484df
Yunus Szönyi <yunus@hellomateo.de>
2026-01-10 23:56:44 +0100

    Update treesitter to latest main branch API
    - Using new require('nvim-treesitter') API
    - Added vimdoc and bash parsers
    - Enabled treesitter-based folding
    - Removed deprecated playground plugin

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-local parsers = { "lua", "go", "javascript", "typescript", "vim", "markdown", "tsx", "json", "html", "css" }
-
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -7,10 +5,12 @@ return {
     build = ":TSUpdate",
     lazy = false,
     config = function()
-      local treesitter = require("nvim-treesitter")
-      treesitter.setup()
-      treesitter.install(parsers)
+      local parsers = { "lua", "go", "javascript", "typescript", "vim", "vimdoc", "markdown", "tsx", "json", "html", "css", "bash" }
 
+      require("nvim-treesitter").setup()
+      require("nvim-treesitter").install(parsers)
+
+      -- Enable treesitter highlighting and indentation
       vim.api.nvim_create_autocmd("FileType", {
         pattern = parsers,
         callback = function()
@@ -18,6 +18,11 @@ return {
           vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
         end,
       })
+
+      -- Enable treesitter-based folding (optional)
+      vim.opt.foldmethod = "expr"
+      vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.opt.foldenable = false -- Start with folds open
     end,
   },
   {
