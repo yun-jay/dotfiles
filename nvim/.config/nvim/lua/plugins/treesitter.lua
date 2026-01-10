2370467e5c262ba102df5e304b996d17dbec2b59
Yunus Szönyi <yunus@hellomateo.de>
2026-01-10 23:48:31 +0100

    Lazy-load treesitter to fix first-time install

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,16 +2,19 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    main = "nvim-treesitter.configs",
-    opts = {
-      ensure_installed = {
-        "lua", "go", "javascript", "typescript", "vim", "markdown",
-        "tsx", "json", "html", "css",
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
-      auto_install = true,
-    },
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSUpdate" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "lua", "go", "javascript", "typescript", "vim", "markdown",
+          "tsx", "json", "html", "css",
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+        auto_install = true,
+      })
+    end,
   },
   {
     "JoosepAlviste/nvim-ts-context-commentstring",
